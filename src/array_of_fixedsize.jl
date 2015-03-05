2274c3aa3092e0430bb4b41c951ead55a8928026
Simon Danisch <sdanisch@gmail.com>
2015-03-05 12:42:12 +0100

    introduced mutable types together with dense array for mutable types

---
+++ src/array_of_fixedsize.jl
@@ -0,0 +1,29 @@
+immutable CArray{T, NDim, ElType} <: DenseArray{T, NDim}
+	data::Array{ElType, NDim}
+end
+
+length{T,N}(A::CArray{T,N}) 			= div(length(A.data), length(T)) 
+endof{T,N}(A::CArray{T,N})  			= length(A)
+size{T,N}(A::CArray{T,N})   			= ntuple(i->size(A,i), N)
+size{T,N}(A::CArray{T,N}, d::Integer) 	= div(size(A.data, d), length(T))
+pointer{T,N}(A::CArray{T,N}) 			= Ptr{T}(pointer(A.data))
+
+function getindex{T,N}(A::CArray{T,N}, i::Integer)
+	unsafe_load(Ptr{T}(pointer(A.data)), i)
+end
+reinterpret{El, N, T}(::Type{T}, A::CArray{El, N, T}) 	= A.data
+reinterpret{T}(::Type{T}, A::CArray) 					= reinterpret(T, A.data)
+
+function Base.vect{T <: FixedArray}(V::T...)
+	ElType = eltype(T)
+	isempty(V) && return CArray{T,1, ElType}()
+	dims = (length(V),length(T))
+	result = Array(ElType, prod(dims))
+	i = 1
+    for v in V, j=1:length(T)
+    	result[i] = v[j]
+    	i+=1
+    end
+    CArray{T, 1, ElType}(result)
+end
+
--- test/test_matrix.jl
+++ test/test_matrix.jl
@@ -1,4 +1,21 @@
-tic()
 using FixedSizeArrays
 
-toc()
+type A{T} <: FixedVector{T, 3}
+	x::T
+	y::T 
+	z::T
+end
+
+function test()
+	a = [A(1,2,3), A(1,2,3)]
+	r = a[1]
+	println(a.data)
+	a = 0
+	r
+end
+
+a = test()
+gc()
+sleep(1)
+gc()
+@show a
