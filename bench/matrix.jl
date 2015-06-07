9bcef6b7debeb6bb792cc7cc09f7e94095363607
Simon Danisch <sdanisch@gmail.com>
2015-06-07 20:53:25 +0200

    some test for map

--- bench/matrix.jl
+++ bench/matrix.jl
@@ -1,50 +1,39 @@
-using ImmutableArrays, FixedSizeArrays
-
-
-
-function test(N, a, b)
-	result = a
-	for i=1:N
-		result = a*b
-	end
-	return result
+function parallelmap{S,T}(f, A::AbstractArray{S}, B::AbstractArray{T})
+    F = similar(A, promote_type(S,T), promote_shape(size(A),size(B)))
+    @parallel for i in eachindex(A,B)
+        @inbounds F[i] = f(A[i], B[i])
+    end
+    return F
 end
 
-
-
-function bench(N)
-
-	const AB 	= Float64[Float64(i*j) for i=1:4, j=1:4]
-	const ABV 	= Float64[1.0, 1.2,2.2, 3.4]
-	const AI 	= Matrix4x4{Float64}(AB)
-	const AFS 	= nvec(AB)
-	const FSV 	= nvec(ABV...)
-	AFS * FSV
-	@show AB * ABV
-
-	@show AFS'
-	@show AB'
-
-	@show AB'
-	bmul  = AB*AB
-	imul  = AI*AI
-	fsmul = matmulfs(AFS,AFS)
-	println(@code_llvm matmulfs(AFS,AFS))
-
-	for i=1:4, j=1:4
-		@assert bmul[i,j] == imul[i,j] == fsmul[i,j]
-	end
-
-	println("julia:")
-	@time result = test(1, AB, AB)
-	@time result = test(N, AB, AB)
-
-	println("Immutables:")
-	@time result = test(1, AI, AI)
-	@time result = test(N, AI, AI)
-	
-	println("Fsa:")
-	@time result = test(1, AFS, AFS)
-	@time result = test(N, AFS, AFS)
+function simplemap{S,T}(f, A::AbstractArray{S}, B::AbstractArray{T})
+    F = similar(A, promote_type(S,T), promote_shape(size(A),size(B)))
+    for i in eachindex(A,B)
+        @inbounds F[i] = f(A[i], B[i])
+    end
+    return F
 end
-bench(10^7)
+
+const a = rand(10^7)
+const b = rand(10^7)
+
+
+@time c = parallelmap(+, a, b)
+@time parallelmap(+, a, b)
+@time d = parallelmap(Base.AddFun(), a, b)
+@time parallelmap(Base.AddFun(), a, b)
+
+
+@time e = simplemap(+, a, b)
+@time simplemap(+, a, b)
+@time f = simplemap(Base.AddFun(), a, b)
+@time simplemap(Base.AddFun(), a, b)
+
+@time g = map(+, a, b)
+@time map(+, a, b)
+@time h = map(Base.AddFun(), a, b)
+@time map(Base.AddFun(), a, b)
+
+@assert c==d && c==e && c==f && c==g && c==h
+
+
