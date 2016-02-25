41ef6318e925acd2262282a25b44c19f65956744
Simon Danisch <sdanisch@gmail.com>
2016-02-26 00:38:29 +0100

    fix #78

--- src/conversion.jl
+++ src/conversion.jl
@@ -6,15 +6,21 @@ function convert{DA <: Array, FSA <: FixedArray}(::Type{DA}, b::FSA)
     result
 end
 
-convert{R, C, R2, C2, T}(a::Type{Mat{R, C, T}}, b::Mat{R2, C2, T}) =
-    Mat(ntuple(c -> ntuple(r -> b[r,c], Val{R}), Val{C}))
+
+convert{FA<:FixedArray}(a::Type{FA}, b::FA) = b
+convert{FA<:Mat}(a::Type{FA}, b::FA) = b
+
+function convert{R, C, R2, C2, T}(MT::Type{Mat{R, C, T}}, b::Mat{R2, C2, T})
+    MT(ntuple(c -> ntuple(r -> b[r,c], Val{R}), Val{C}))
+end
 
 #conversion
 convert{T <: Tuple}(::Type{T}, x::Real)                   = ntuple(ConstFunctor(eltype(T)(x)), Val{length(T.parameters),})
-convert{T <: Tuple, FSA <: FixedArray}(::Type{T}, x::FSA) = map(eltype(T), x.(1)[1:length(T.parameters)])
-convert{T <: FixedArray}(t::Type{T}, f::T)                = f
-convert{FSA1 <: FixedArray}(t::Type{FSA1}, f::FixedArray) =
+convert{T <: Tuple, FSA <: FixedArray}(::Type{T}, x::FSA) = convert(T, get_tuple(x))
+
+function convert{FSA1 <: FixedArray}(t::Type{FSA1}, f::FixedArray)
     map(ConversionIndexFunctor(f, eltype_or(FSA1, eltype(typeof(f)))), FSA1)
+end
 
 convert{T <: FixedArray}(::Type{T}, x) = T(x)
 convert{T <: FixedArray}(::Type{T}, x...) = T(x)
