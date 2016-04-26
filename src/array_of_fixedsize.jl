567ab86b3003dca8c908219cff0e9cb9f808b2d7
SimonDanisch <sdanisch@gmail.com>
2016-04-26 10:38:03 +0200

    some speed improvements

--- src/array_of_fixedsize.jl
+++ src/array_of_fixedsize.jl
@@ -8,10 +8,15 @@ function show{FSA <: FixedVector}(io::IO, a::Vector{FSA})
 end
 immutable MaxFun <: Func{2} end
 immutable MinFun <: Func{2} end
+immutable ExtremaFun <: Func{2} end
 call(::MaxFun, a, b) = max(a, b)
 call(::MinFun, a, b) = min(a, b)
-minimum{T <: FixedArray}(a::Vector{T}) = reduce(MinFun(), a)
-maximum{T <: FixedArray}(a::Vector{T}) = reduce(MaxFun(), a)
+call(::ExtremaFun, reducevalue, a) = min(reducevalue[1], a), max(reducevalue[2], a)
+
+
+minimum{T <: FixedArray}(a::AbstractVector{T}) = reduce(MinFun(), a)
+maximum{T <: FixedArray}(a::AbstractVector{T}) = reduce(MaxFun(), a)
+Base.extrema{T <: FixedArray}(a::AbstractVector{T}) = reduce(ExtremaFun(), a)
 function isapprox{FSA <: FixedArray, A <: Union{Array, FixedArray}}(a::FSA, b::A)
     for i=1:length(a)
         !isapprox(a[i], b[i]) && return false
