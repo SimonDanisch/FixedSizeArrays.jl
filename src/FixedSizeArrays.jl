936a155a68abcadb40b87c70df0d56cc483215c4
Chris Foster <chris42f@gmail.com>
2016-05-02 23:17:59 +1000

    Additional methods for similar() to mirror Base

--- src/FixedSizeArrays.jl
+++ src/FixedSizeArrays.jl
@@ -18,13 +18,15 @@ if VERSION <= v"0.5.0"
 end
 
 # put them here due to #JuliaLang/julia#12814
-# needs to be befor indexing and ops, but after constructors
+# needs to be before indexing and ops, but after constructors
 immutable Mat{Row, Column, T} <: FixedMatrix{Row, Column, T}
     _::NTuple{Column, NTuple{Row, T}}
 end
 function similar{FSA <: Mat, T}(::Type{FSA}, ::Type{T}, SZ::NTuple{2, Int})
     Mat{SZ[1], SZ[2], T}
 end
+similar{FSA <: Mat}(::Type{FSA}, SZ::NTuple{2,Int}) = similar(FSA, eltype(FSA), SZ)
+similar{N,M,S, T}(::Type{Mat{N,M,S}}, ::Type{T}) = Mat{N,M,T}
 
 # most common FSA types
 immutable Vec{N, T} <: FixedVector{N, T}
